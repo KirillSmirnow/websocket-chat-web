9e4d0f0c44f110598ad4479311b74d36c169d5e1
Kirill Smirnov <smirnowku@gmail.com>
2022-11-29 17:54:59 +0300

    Messages

--- src/main/kotlin/component/Messages.kt
+++ src/main/kotlin/component/Messages.kt
@@ -26,112 +26,139 @@ external interface MessagesProps : Props {
 }
 
 val Messages = FC<MessagesProps> { props ->
+    val selectedChat = props.chat
     var initializedChat: Chat? by useState(null)
     var messages: List<Message> by useState(emptyList())
     var user: User? by useState(null)
+    var showInvitationCode: Boolean by useState(false)
     GlobalScope.launch {
-        val chat = props.chat
-        if (chat != null && chat != initializedChat) {
-            messages = getChatMessages(props.session, chat).content.reversed()
-            initializedChat = chat
+        if (selectedChat != null && selectedChat != initializedChat) {
+            messages = getChatMessages(props.session, selectedChat).content.reversed()
+            initializedChat = selectedChat
         }
-    }
-    GlobalScope.launch {
         if (user == null) {
             user = getCurrentUser(props.session)
         }
     }
 
     div {
-        +(props.chat?.name ?: "Select Chat")
-        css {
-            height = 20.px
-            padding = 10.px
-            borderWidth = LineWidth.thin
-            borderBottomStyle = LineStyle.solid
-            fontWeight = FontWeight.bold
-        }
-    }
-    if (messages.isEmpty()) {
         div {
-            img {
-                src = "duck.png"
-                width = 300.0
+            div {
+                +(selectedChat?.name ?: "Select Chat")
+                css {
+                    fontWeight = FontWeight.bold
+                    flexGrow = number(1.0)
+                }
             }
-            css {
-                textAlign = TextAlign.center
-            }
-        }
-    } else {
-        div {
-            for (message in messages) {
-                div {
-                    if (message.sender != user) {
-                        div {
-                            +message.sender.name
-                            css {
-                                fontStyle = FontStyle.oblique
-                                fontSize = 11.pt
-                            }
-                        }
-                    }
-                    +message.text
-                    css {
-                        width = Length.fitContent
-                        margin = 4.px
-                        padding = 4.px
-                        borderStyle = LineStyle.groove
-                        borderWidth = LineWidth.thin
-                        borderRadius = 10.px
-                        if (message.sender == user) {
-                            backgroundColor = Color("LemonChiffon")
+            if (selectedChat != null) {
+                if (showInvitationCode) {
+                    div {
+                        +selectedChat.invitationCode
+                        css {
+                            marginRight = 10.px
                         }
                     }
                 }
+                button {
+                    +(if (showInvitationCode) "Hide Invitation Code" else "Show Invitation Code")
+                    onClick = { showInvitationCode = !showInvitationCode }
+                }
             }
             css {
-                height = 50.pct
-                padding = 4.px
-                overflowY = Overflow.scroll
+                height = 20.px
+                padding = 10.px
+                display = Display.flex
+                borderWidth = LineWidth.thin
+                borderBottomStyle = LineStyle.solid
             }
         }
-    }
-    val chat = props.chat
-    if (chat != null) {
-        val messageSending = MessageSending(null)
-        div {
+        if (messages.isEmpty()) {
+            div {
+                img {
+                    src = "duck.png"
+                    width = 300.0
+                }
+                css {
+                    textAlign = TextAlign.center
+                    flexGrow = number(1.0)
+                }
+            }
+        } else {
+            div {
+                for (message in messages) {
+                    div {
+                        if (message.sender == user) {
+                            div {
+                                css {
+                                    flexGrow = number(1.0)
+                                }
+                            }
+                        }
+                        div {
+                            if (message.sender != user) {
+                                div {
+                                    +message.sender.name
+                                    css {
+                                        fontStyle = FontStyle.oblique
+                                        fontSize = 11.pt
+                                    }
+                                }
+                            }
+                            +message.text
+                            css {
+                                width = Length.fitContent
+                                maxWidth = 400.px
+                                margin = 4.px
+                                padding = 4.px
+                                borderStyle = LineStyle.groove
+                                borderWidth = LineWidth.thin
+                                borderRadius = 10.px
+                                if (message.sender == user) {
+                                    backgroundColor = Color("LemonChiffon")
+                                }
+                            }
+                        }
+                        css {
+                            display = Display.flex
+                        }
+                    }
+                }
+                css {
+                    padding = 4.px
+                    flexGrow = number(1.0)
+                    overflowY = Overflow.scroll
+                }
+            }
+        }
+        if (selectedChat != null) {
+            val messageSending = MessageSending(null)
             div {
                 input {
                     onChange = { event -> messageSending.text = event.target.value }
                     css {
-                        width = 100.pct
+                        flexGrow = number(1.0)
                     }
                 }
-                css {
-                    width = 420.px
-                    display = Display.inlineBlock
-                    paddingRight = 10.px
-                }
-            }
-            div {
                 button {
                     +"|>"
                     onClick = {
                         GlobalScope.launch {
-                            sendMessage(props.session, chat, messageSending)
+                            sendMessage(props.session, selectedChat, messageSending)
                         }
                     }
                 }
                 css {
-                    width = 30.px
-                    display = Display.inlineBlock
-                    paddingLeft = 10.px
+                    display = Display.flex
+                    padding = 10.px
+                    borderWidth = LineWidth.thin
+                    borderTopStyle = LineStyle.solid
                 }
             }
-            css {
-                padding = 5.px
-                textAlign = TextAlign.center
-            }
+        }
+        css {
+            height = 100.pct
+            display = Display.flex
+            flexDirection = FlexDirection.column
         }
     }
 }
